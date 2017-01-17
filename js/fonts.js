363477a26ca6f8167bf67dfad6ce6c2de3c58938
Abhishek Warokar <abhishek11@live.in>
2017-01-17 22:18:58 +0530

    #Day3

--- js/fonts.js
+++ js/fonts.js
@@ -5,15 +5,19 @@
  */
 var combo = [
     [
-        ["Playfair Display", "900i,400", 2.111, "#AA9585", "Open Sans", 400, 1.111, "#FDFDFD", "#0A0A0A"],
-        ["Abril Fatface", 400, 2.111, "#AFFFCC", "Work Sans", 400, 1, "#FFFFFF", "#183F63"],
-        ["Copse", 400, 2.111, "#F85658", "Merriweather", 400, 1, "#FDFDFD", "#222222"]
+        ["Roboto Condensed", 400, 2.24, "#4D3EEE", "Roboto Slab", 400, 1.11, "#4B2B55", "#FCDCEB"],
+        ["Vidaloka", 400, 2.11, "#F0CF61", "Overpass", 400, 1.11, "#EAEAEA", "#2C3979"],
+        ["Dosis", 700, 2.11, "#F58580", "Cabin", 400, 1.2, "#343434", "#FFFFF2"],
     ],
     [
-        ["Poppins", 700, 2.111, "#333333", "Rubik", 400, 1.111, "#7E8892", "#FFFFFF"],
+        ["Playfair Display", "900i,400", 2.11, "#AA9585", "Open Sans", 400, 1.11, "#FDFDFD", "#0A0A0A"],
+        ["Abril Fatface", 400, 2.11, "#AFFFCC", "Work Sans", 400, 1, "#FFFFFF", "#183F63"],
+        ["Copse", 400, 2.11, "#F85658", "Merriweather", 400, 1, "#FDFDFD", "#222222"]
+    ],
+    [
+        ["Poppins", 700, 2.11, "#333333", "Rubik", 400, 1.11, "#7E8892", "#FFFFFF"],
         ["Playfair Display", 700, 2, "#253445", "Fauna One", 400, 1, "#394655", "#FFFEF7"],
-        ["Trocchi", 400, 2.111, "#D14B4C", "Open Sans", 400, 1.111, "#B47880", "#321B30"]
+        ["Trocchi", 400, 2.11, "#D14B4C", "Open Sans", 400, 1.11, "#B47880", "#321B30"]
     ]
-    // ["BioRhyme", 400, 2.111, "#0A0A0A", "Montserrat", 400, 1.111, "#343434", "#FF7675"],
 ];
-var dayCount = 2;
+var dayCount = combo.length;
