e4b57e47f1bf0f002cdbcc7171744d927e132e67
Abhishek Warokar <abhishek11@live.in>
2017-01-20 19:17:26 +0530

    trail , rm

--- js/fonts.js
+++ js/fonts.js
@@ -7,22 +7,22 @@ var combo = [
     [
         ["Bree Serif", 400, 2.24, "#773344", "Libre Franklin", 400, 1.11, "#0B0014", "#F5E9E2"],
         ["Open Sans", "700", 2.11, "#7161EF", "Fira Sans", 400, 1.11, "#FDFDFD", "#111111"],
-        ["Eczar", 700, 2.1, "#EF233C", "Alegreya", 400, 1.11, "#8D99AE", "#2B2D42"],
+        ["Eczar", 700, 2.1, "#EF233C", "Alegreya", 400, 1.11, "#8D99AE", "#2B2D42"]
     ],
     [
         ["Nunito", 700, 2.11, "#00C76C", "Arsenal", 400, 1.2, "#FDFDFD", "#1F2427"],
         ["Bitter", 400, 2.24, "#FF654B", "Lora", 400, 1.2, "#2A2D66", "#EFDB9F"],
-        ["Quicksand", "700", 2.11, "#AFFFCC", "Open Sans", 400, 1.11, "#00D1AB", "#1D1856"],
+        ["Quicksand", "700", 2.11, "#AFFFCC", "Open Sans", 400, 1.11, "#00D1AB", "#1D1856"]
     ],
     [
         ["Elsie", "400", 2.24, "#F58580", "Lato", 400, 1.11, "#775555", "#FEE1DB"],
         ["Josefin Sans", 700, 2.11, "#3366CC", "Lora", 400, 1.11, "#343434", "#F6F6F6"],
-        ["Muli", 400, 2.11, "#FDFDFD", "PT Sans", 400, 1.11, "#EDEEEB", "#253239"],
+        ["Muli", 400, 2.11, "#FDFDFD", "PT Sans", 400, 1.11, "#EDEEEB", "#253239"]
     ],
     [
         ["Roboto Condensed", 400, 2.24, "#4D3EEE", "Roboto Slab", 400, 1.11, "#4B2B55", "#FCDCEB"],
         ["Vidaloka", 400, 2.11, "#F0CF61", "Overpass", 400, 1.11, "#EAEAEA", "#2C3979"],
-        ["Dosis", 700, 2.11, "#F58580", "Cabin", 400, 1.2, "#343434", "#FFFFF2"],
+        ["Dosis", 700, 2.11, "#F58580", "Cabin", 400, 1.2, "#343434", "#FFFFF2"]
     ],
     [
         ["Playfair Display", "900i,400", 2.11, "#AA9585", "Open Sans", 400, 1.11, "#FDFDFD", "#0A0A0A"],
